fb504f452be5b058074f166653924822615ef496
Christian <christiangouw168@gmail.com>
2020-02-19 18:56:05 +0700

    menambahkan file aplikasi

---
+++ src/js/main.js
@@ -0,0 +1,176 @@
+function getPage(page) {
+    var ajax = new XMLHttpRequest();
+    ajax.open("get",page,true);
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            document.getElementById("mainPage").innerHTML = ajax.responseText;      
+        }
+    }
+    ajax.send();
+}
+
+function test(){
+    console.log("berhasil")
+}
+
+// absen masuk
+
+function reedem(){
+    Swal.fire({
+        icon : "success",
+        titlle : "Kupon Sudah Ditambahkan !",
+        html : 
+        "<b>Kupon berhasil ditambahka</b>"+
+        "<div class='text-muted font-size-small mt-3'>Selamat anda sudah berhasil menukarkan point anda , cek kupon anda sekarang</div>",
+        showCloseButton : true,
+        confirmButtonText : "CEK KUPON"
+    }).then((result)=>{
+        if(result.value){
+            getPage('cekVoucher.html');
+        }
+    })
+}
+
+function absenMasuk(){
+
+    var offline = window.onoffline;
+
+    if(window == offline){
+        offlineAlert();
+    }else{
+        Swal.fire({
+            icon : "success",
+            title : "Anda Berhasil !",
+            html:
+                '<b>Point Anda +10</b> <br> ' +
+                '<div class="text-muted font-size-small mt-3">Selamat anda sudah berhasil absen buat hari ini , nih bonus buat kamu , semagat ya !</div>',
+            showCloseButton : true,
+            confirmButtonText : "CEK POINT"
+        }).then((result)=>{
+            if (result.value){
+                // location.href="cekPoint.html";  
+                getPage('cekPoint.html');
+            }
+        })
+    }
+
+}
+
+// end absen masuk
+
+// offline
+
+function offlineAlert(){
+    Swal.fire({
+        icon : "error",
+        title : "Anda sedang offline !",
+        html: 
+        "<b>Tidak ada sambungan</b>"+
+        "<div>Silahkan cek sambungan anda untuk melanjutkan</div>",
+    })
+}
+
+// end offline
+
+// ijin terkirim
+
+function ijinTerkirim(){
+    Swal.fire({
+        icon : "success",
+        title : "berhasil mengirim",
+        html : "<div class='text-muted font-size-small mt-3'>semoga lekas sembuh</div>",
+        confirmButtonText :"Ok"
+    })
+}
+
+// end ijin terkirim
+
+// get page
+
+function getPage(page){
+    var ajax = new XMLHttpRequest();    
+    ajax.open("get",page,true);
+    ajax.onreadystatechange = function(){
+        if(ajax.status == 200 && ajax.readyState==4){
+            document.getElementById("main").innerHTML = ajax.responseText;
+        }
+    }
+    ajax.send();
+}
+
+// end get page
+
+// include tamplate
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;    
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];      
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {        
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                if (this.status == 404) {elmnt.innerHTML = "Page not found.";}            
+                elmnt.removeAttribute("w3-include-html");
+                includeHTML();
+            }
+        }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
+
+includeHTML();
+
+// end include tamplate
+
+// link to 
+    function redirect(page){
+        location.href = page;
+    }
+// end link 
+
+// open camera
+
+function camera(){
+    
+    const player = document.getElementById("player");
+    const captureButton = document.getElementById("capture-button");
+    const outputCanvas = document.getElementById("output");
+    const context = outputCanvas.getContext("2d");
+
+    captureButton.style = "block";
+
+    navigator.mediaDevices
+    .getUserMedia({video:true})
+    .then((stream)=>{
+        player.srcObject = stream;
+    }).catch(error=>{
+        console.log("error")
+    })
+
+    captureButton.addEventListener('click',function(){
+        const imageHeight = player.offsetHeight;
+        const imageWidth = player.offsetWidth;
+
+        outputCanvas.width = imageWidth;
+        outputCanvas.height = imageHeight;
+
+        context.drawImage(player,0,0,imageWidth,imageHeight);
+
+        const downloadLink = document.createElement('a');
+        downloadLink.setAttribute('download','filename.png');
+
+        outputCanvas.toBlob((blob)=>{
+            downloadLink.setAttribute('href',URL.createObjectURL(blob));
+            downloadLink.click();
+        });
+    });
+}
+
+// end open camera
---
+++ sw.js
@@ -0,0 +1,122 @@
+// file cache
+const file = [
+    "/",    
+
+    "./src/img/008-salary.png",
+    "./src/img/011-salary.png",
+    "./src/img/014-name.png",
+    "./src/img/017-user.png",
+    "./src/img/027-calendar.png",
+    "./src/img/035-calendar.png",
+    "./src/img/BANNER_IDBYTE_2.jpg",
+    "./src/img/code.png",
+    "./src/img/JOOX-VIP-Gojek-T1-Banner.jpg",
+    "./src/img/man.png",
+    "./src/img/suitcase-2.png",
+    "./src/img/undraw_add_notes_8cfw.png",
+    "./src/img/undraw_discount_d4bd.png",
+    "./src/img/undraw_joyride_hnno.png",
+    "./src/img/undraw_learning_2q1h.png",
+    "./src/img/undraw_mobile_login_ikmv.png",
+    "./src/img/undraw_noted_pc9f.png",
+    "./src/img/undraw_printing_invoices_5r4r.png",
+    "./src/img/undraw_product_teardown_elol.png",
+    "./src/img/undraw_Savings_dwkw.png",
+    "./src/img/undraw_updated_rr85.png",
+    "./src/img/user.png",
+
+
+    "./pages/main.html",
+    "./pages/akun.html",
+    "./pages/cekPoint.html",
+    "./pages/cekTugas.html",
+    "./pages/cekVoucher.html",
+    "./pages/cuti.html",
+    "./pages/home.html",
+    "./pages/ijin.html",
+    "./pages/info.html",
+    "./pages/pengajuanIjin.html",
+    "./pages/pangajuanCuti.html",
+    "./pages/register.html",
+    "./pages/slipGaji.html",
+    "./pages/useVoucher.html",
+    "./pages/login.html",
+    "./pages/camera.html",
+    
+    "./material/camera.html",
+
+    "./material/footer.html",
+    "./material/header.html",    
+
+    "./src/css/app.css",
+    "./src/css/bootstrap.css",    
+    
+    "./src/js/main.js",
+    "./src/js/sweetalert.min.js",  
+    "./src/js/w3.js",      
+    "./src/js/jquery.min.js",      
+    "./src/js/bootstrap.min.js",        
+    "./src/js/popper.min.js",     
+    
+    "./sw.js",
+    "./manifest.json",
+]
+
+const cacheStaticName = "siabsen-mainapp";
+
+self.addEventListener('install',function(event){
+    console.log("service worker intalled")
+    event.waitUntil(
+        caches.open(cacheStaticName)
+        .then(function(cache){
+            return cache.addAll(file);
+        })        
+    );
+})
+
+self.addEventListener('fetch', function(event) {
+    console.log("fetch url : " , event.request.url)
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response){
+            console.log("found in cache")
+            if(response){
+                return response;
+            }
+            return fetch(event.request)
+            .then(function(response){
+                if(response.status == 404){
+                    return caches.match("404.html");
+                }
+
+                return caches.open(cacheStaticName)
+                .then(function(chace){
+                    cache.put(event.request.url , response.clone());
+                    return response;
+                })
+
+            })
+            .catch(function(error){
+                return caches.match("offline.html");
+            })
+        })
+    )
+});
+
+self.addEventListener('activate',function(event){
+    const allChace = [cacheStaticName]
+    event.waitUntil(
+        caches.keys()
+        .then(function(cachesNames){
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhiteList.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        })
+    )
+})
+
+
